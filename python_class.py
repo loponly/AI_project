1068c396001f2bb58aec5176d65cfb8a77868359
Enkhbat Enkhtaivan (IST) <EnkhbatE@riotinto.com>
2019-04-26 14:18:08 +0800

    20190426@1418

---
+++ python_class.py
@@ -0,0 +1,38 @@
+
+
+# List bols array yum a
+
+
+numbers = list()
+
+for i in range(100):
+    numbers.append(i)
+# append list deer add hij  baigaa m a
+
+
+#dict array bugderee key tei yum a
+person = dict()
+
+
+person['Bayrtsogt']='Database Expert'
+person['Enkhbat']='Python Programist'
+
+print(person)
+
+
+
+# Function 
+
+def funcname(self, parameter_list):
+    pass
+    return 1
+
+
+class className:
+    def __init__():
+        pass
+    
+    def __del__():
+        pass
+
+# Manisaa bolhgui bol asugaraa 
